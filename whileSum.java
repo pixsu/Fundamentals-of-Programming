77b02690b4949cf599a783f565721abca40cf4cd
Angelo Timothy L. Picart <113318625+pixsu@users.noreply.github.com>
2022-11-06 21:48:27 +0800

    Update whileSum.java
    Create a program that makes the user input positive integers, the program will only end if the user inputs a negative integer.
    Print only the sum of the positive integers entered by the user (ignore the negative integer) using while loop only.

--- whileSum.java
+++ whileSum.java
@@ -18,5 +18,6 @@ public class whileSum {
         
     }
 }
+
 //Create a program that makes the user input positive integers, the program will only end if the user inputs a negative integer.
-//Print only the sum of the positive integers entered by the user(ignore the negative integer)
+//Print only the sum of the positive integers entered by the user (ignore the negative integer) using while loop only.
