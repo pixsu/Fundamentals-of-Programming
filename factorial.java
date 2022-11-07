5e854f745a51ec1b11c396e934cd449ad07644ac
Angelo Timothy L. Picart <113318625+pixsu@users.noreply.github.com>
2022-11-07 11:51:12 +0800

    Create factorial.java

---
+++ factorial.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class javaAssignment2 {
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+
+    double result = 1;
+
+    System.out.print("Enter number: ");
+    int base = sc.nextInt();
+    System.out.print("Enter number: ");
+    int exponent=sc.nextInt();
+    
+    
+    for(;exponent !=0; --exponent){
+        result = base * result;
+    }
+    System.out.println("Result: "+result);
+    
+}
+}
+//Assignment #2
+//user will input two numbers, create a program to calculate the value of an integer(1st input) raised to a certain power(2nd input) 
