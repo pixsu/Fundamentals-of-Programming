299bb595d5c6168112cb9d511f75e14bb9f9a2fe
Angelo Timothy L. Picart <113318625+pixsu@users.noreply.github.com>
2022-11-07 12:10:31 +0800

    Update DoWhileSum.java

--- DoWhileSum.java
+++ DoWhileSum.java
@@ -1,21 +1,18 @@
 import java.util.*;
 public class DoWhileSum {
-    public static void main(String[] args){
-
+    public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
-
-        int num=0, sum=0;
-
-        do {
-            System.out.print("Enter number: ");
-            num =sc.nextInt();
-            if(num>=0)
-            {
-                sum=num+sum;
-            }
+        int num=0,sum=0;
+       
+        do{
+            sum += num;
+            System.out.println("Enter number: ");
+            num=sc.nextInt();
         }
         while(num>=0);
-        System.out.println("Sum: "+sum);
+        System.out.println("Sum: "+ sum);
+
     }
 }
 //Create a program that makes the user input positive integers, the program will only end if the user inputs a negative integer.
