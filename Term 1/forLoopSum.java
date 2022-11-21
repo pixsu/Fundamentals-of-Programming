0536777bae0718b14f35d4712baa7234ded37b1c
Angelo Timothy L. Picart <113318625+pixsu@users.noreply.github.com>
2022-11-21 21:02:47 +0800

    Create forLoopSum.java

---
+++ Term 1/forLoopSum.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class forLoopSum {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int num,n1=0,sum=0;
+
+        System.out.print("Enter number: ");
+        num = sc.nextInt();
+
+        for (n1=0; num>=n1;)
+        {
+            sum = num + sum;
+            System.out.print("Enter number: ");
+            num = sc.nextInt();
+        }
+        System.out.println("Sum: "+sum);
+
+    }
+}
+//Create a program that makes the user input positive integers, the program will only end if the user inputs a negative integer.
+//Print only the sum of the positive integers entered by the user (ignore the negative integer) using for loop only
